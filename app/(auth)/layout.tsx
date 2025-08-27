e1484c7a8397a2267a7be9cf840043c3f0acf2a8
Pranav2845 <pranavpandey9211@gmail.com>
2025-08-27 20:21:49 +0530

    fix: add dark mode support for icons

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -30,7 +30,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         </div>
       </section>
 
-      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+        <section className="flex flex-1 flex-col items-center py-10 px-4 bg-white dark:bg-dark-200 dark:text-light-200 lg:justify-center lg:px-10 lg:py-0">
         <div className="mb-16 lg:hidden">
           <Image
             src="/assets/icons/logo-full-brand.svg"
